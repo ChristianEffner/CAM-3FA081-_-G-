f02dfe79f5113c41a8b3559ce3a856e41995f6dc
Christian.Effner <ch.effner@t-online.de>
2024-10-10 12:51:58 +0200

    added classes for interfaces

---
+++ src/main/java/interfaces/Costumer.java
@@ -0,0 +1,52 @@
+package interfaces;
+
+import enums.Gender;
+
+import java.time.LocalDate;
+
+public class Costumer implements ICustomer {
+
+    private String firstName;
+    private String lastName;
+
+
+    @Override
+    public void setFirstName(String firstName) {
+
+    }
+
+    @Override
+    public void setLastName(String lastname) {
+
+    }
+
+    @Override
+    public void setBirthDate(LocalDate birthDate) {
+
+    }
+
+    @Override
+    public void setGender(Gender gender) {
+
+    }
+
+    @Override
+    public String getFirstName() {
+        return "";
+    }
+
+    @Override
+    public String getLastName() {
+        return "";
+    }
+
+    @Override
+    public LocalDate getBirthDate() {
+        return null;
+    }
+
+    @Override
+    public Gender getGender() {
+        return null;
+    }
+}
---
+++ src/main/java/interfaces/ID.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+import java.util.UUID;
+
+public class ID implements IID{
+    @Override
+    public UUID getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(UUID id) {
+
+    }
+}
